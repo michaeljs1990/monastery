a5697fee214a266001de119979113fc0bae8c326
Michael Schuett <michaeljs1990@gmail.com>
2018-04-03 01:50:53 -0400

    Names...
    Changeup names so they make more sense. also add some comments about wtf
    this code actually is used for.

--- src/storage/s3.go
+++ src/storage/s3.go
@@ -18,6 +18,21 @@ type S3 struct {
 	config aws.Config
 }
 
+// ResponseWriterFake is a struct that provides a WriteAt method to fake like it is
+// a file livel abstraction that we can write to. This is because the S3 API needs to
+// implement the WriteAt interface. We fake this and limit the concurrency to one so that
+// bytes are written in the proper order to the requesting client.
+type ResponseWriterFake struct {
+	http.ResponseWriter
+}
+
+// TODO: I am a bad person and this does bad things.
+func (w ResponseWriterFake) WriteAt(b []byte, off int64) (n int, err error) {
+	// fmt.Println("bytes: " + strconv.Itoa(len(b)))
+	// fmt.Println("offset: " + strconv.FormatInt(off, 10))
+	return w.Write(b)
+}
+
 // LoadConfig handles the needed credentials for connecting to the desired
 // aws account and bucket for file uploads.
 func (s *S3) LoadConfig() {
@@ -49,17 +64,6 @@ func (s *S3) WriteFile(f AbstractFile) (err error) {
 	return err
 }
 
-type Tests struct {
-	http.ResponseWriter
-}
-
-// TODO: I am a bad person and this does bad things.
-func (w Tests) WriteAt(b []byte, off int64) (n int, err error) {
-	// fmt.Println("bytes: " + strconv.Itoa(len(b)))
-	// fmt.Println("offset: " + strconv.FormatInt(off, 10))
-	return w.Write(b)
-}
-
 // ReadFile from S3 and write to the client
 func (s *S3) ReadFile(f AbstractFile, w http.ResponseWriter) error {
 	// Since the client isn't able to write files with offset
@@ -68,7 +72,7 @@ func (s *S3) ReadFile(f AbstractFile, w http.ResponseWriter) error {
 	downloader := s3manager.NewDownloader(s.config)
 	downloader.Concurrency = 1
 
-	t := Tests{w}
+	t := ResponseWriterFake{w}
 	_, err := downloader.Download(t, &s3.GetObjectInput{
 		Bucket: aws.String(config.S3Bucket),
 		Key:    aws.String(f.Name),
